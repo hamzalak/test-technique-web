7563855f6e4a1f04e747e6c8d1ed06e73010626d
LAKHAL Hamza <46263489+hamzalak@users.noreply.github.com>
2021-12-13 17:44:41 +0100

    adding intial states and selectors

--- src/app/redux/equipements/selectors.ts
+++ src/app/redux/equipements/selectors.ts
@@ -4,6 +4,41 @@ export function getTheActualEquipment(state: GlobalTypeState) {
   return state["equipements"].requestedItem;
 }
 
+export function getAllEquipementsAsArray(state: GlobalTypeState) {
+  const equipements = state["equipements"].items;
+  return Object.keys(equipements).map((key) => ({
+    equipementKey: key,
+    ...equipements[key],
+  }));
+}
+
+export function getEquipementByOption(
+  state: GlobalTypeState,
+  textAnOption: { option: "name" | "domain"; text: string }
+) {
+  const equipements = state["equipements"].items;
+  const { text, option } = textAnOption;
+
+  if (!text) return getAllEquipementsAsArray(state);
+
+  return Object.keys(equipements).reduce((prev, act) => {
+    if (
+      equipements[act][option]
+        .toLocaleLowerCase()
+        .includes(text.toLocaleLowerCase())
+    ) {
+      return [
+        ...prev,
+        {
+          equipementKey: act,
+          ...equipements[act],
+        },
+      ];
+    }
+    return prev;
+  }, [] as any);
+}
+
 export function getAllEquipements(state: GlobalTypeState) {
   return state["equipements"].items;
 }
