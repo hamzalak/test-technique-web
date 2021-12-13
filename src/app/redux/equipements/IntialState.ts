de40b8251761f43353334bbdcbbcc686f878e4b4
LAKHAL Hamza <46263489+hamzalak@users.noreply.github.com>
2021-12-13 17:44:18 +0100

    adding intial states and selectors

--- src/app/redux/equipements/IntialState.ts
+++ src/app/redux/equipements/IntialState.ts
@@ -2,18 +2,18 @@ import { Equipement } from "../../../model/Equipement";
 import { GlobalTypeState } from "../GlobalStateType";
 
 const intialEquipement: Equipement = {
-  name: " ",
-  building: " ",
-  domain: " ",
-  niveau: " ",
-  local: " ",
-  photo: " ",
-  brand: " ",
-  model: " ",
+  name: "",
+  building: "",
+  domain: "",
+  niveau: "",
+  local: "",
+  photo: "",
+  brand: "",
+  model: "",
   serialNumber: "",
   quantity: 0,
   status: "Fonctionnel",
-  notes: " ",
+  notes: "",
   nbFaults: 0,
 };
 
