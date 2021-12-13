46b28eb4392871878b5e33e024cdd3ec75333253
LAKHAL Hamza <46263489+hamzalak@users.noreply.github.com>
2021-12-13 17:46:06 +0100

    modification to equipement components

--- src/app/Equipements/EquipementDescription.tsx
+++ src/app/Equipements/EquipementDescription.tsx
@@ -1,10 +1,128 @@
-import { useSelector } from "react-redux";
+import { Badge, Button, Card, Col, Descriptions, Divider, Row } from "antd";
+import Meta from "antd/lib/card/Meta";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useLoadEquipements } from "../hooks/useLoadEquipements";
+import { ActionEnum } from "../redux/equipements/DispatchType";
 import { getTheActualEquipment } from "../redux/equipements/selectors";
+import { CheckpointsTable } from "../Checkpoints/CheckpointsTable";
+import { ToolTwoTone } from "@ant-design/icons";
+import { database } from "../../firebase";
+import { Checkpoint } from "../../model/Checkpoint";
 
 export function EquipementDescription() {
-  const elem = useSelector(getTheActualEquipment);
+  const equipement = useSelector(getTheActualEquipment);
 
-  console.log(elem);
+  const [checkpointVisible, setCheckponitVisible] = useState(false);
 
-  return <div>juhjkhg</div>;
+  const onCloseCheckpoints = () => setCheckponitVisible(false);
+
+  const onOpenCheckpoints = () => setCheckponitVisible(true);
+
+  const [checkPoints, setCheckpoints] = useState<Checkpoint[]>();
+
+  useLoadEquipements();
+
+  const dispatch = useDispatch();
+
+  const { equipementId } = useParams() as { equipementId: string };
+
+  useEffect(() => {
+    const fetcheAllCheckpoints = async () => {
+      try {
+        const checkpointsSnapshot = await database
+          .ref("Checkpoints")
+          .orderByChild("equipmentKey")
+          .equalTo(equipementId)
+          .once("value");
+        const data = [] as Checkpoint[];
+        checkpointsSnapshot.forEach((childSnapshot) => {
+          data.push(childSnapshot.val());
+        });
+        setCheckpoints(data);
+      } catch (e) {
+        // Error happened
+      }
+    };
+    fetcheAllCheckpoints();
+  }, [equipementId]);
+
+  useEffect(() => {
+    if (!equipement || !equipement?.name) {
+      dispatch({
+        type: ActionEnum.FETCH_EQUIPEMENT,
+        payload: equipementId,
+      });
+    }
+  });
+
+  const {
+    name,
+    building,
+    niveau,
+    local,
+    domain,
+    brand,
+    serialNumber,
+    quantity,
+    notes,
+    status,
+    photo,
+  } = equipement || {};
+
+  const statusColor = status === "En Marche" ? "processing" : "success";
+
+  const TiltleComponent = () => (
+    <div>
+      {" "}
+      <ToolTwoTone /> Détails{" "}
+    </div>
+  );
+
+  return (
+    <>
+      <Divider
+        style={{
+          marginTop: "100px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Button onClick={onOpenCheckpoints}>Afficher Les Checkpoints</Button>
+        <CheckpointsTable
+          visible={checkpointVisible}
+          onClose={onCloseCheckpoints}
+          checkpoints={checkPoints}
+        />
+      </Divider>
+      <Row>
+        <Col span={12}>
+          <Card hoverable cover={<img alt="example" src={photo} />}>
+            <Meta title={name} />
+          </Card>
+        </Col>
+        <Col span={12} style={{ paddingLeft: "10px" }}>
+          <Descriptions title={<TiltleComponent />} bordered>
+            <Descriptions.Item label="Nom">{name}</Descriptions.Item>
+            <Descriptions.Item label="Batiment">{building}</Descriptions.Item>
+            <Descriptions.Item label="Niveau">{niveau}</Descriptions.Item>
+            <Descriptions.Item label="Local">{local}</Descriptions.Item>
+            <Descriptions.Item label="Domaine" span={2}>
+              {domain}
+            </Descriptions.Item>
+            <Descriptions.Item label="Status" span={3}>
+              <Badge status={statusColor} text={status} />
+            </Descriptions.Item>
+            <Descriptions.Item label="Marque">{brand}</Descriptions.Item>
+            <Descriptions.Item label="Numéro de série">
+              {serialNumber}
+            </Descriptions.Item>
+            <Descriptions.Item label="Quantité">{quantity}</Descriptions.Item>
+            <Descriptions.Item label="Notes">{notes}</Descriptions.Item>
+          </Descriptions>
+        </Col>
+      </Row>
+    </>
+  );
 }
--- src/app/Equipements/EquipementsTable.tsx
+++ src/app/Equipements/EquipementsTable.tsx
@@ -1,30 +1,131 @@
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllEquipements } from "../redux/equipements/selectors";
+import { getEquipementByOption } from "../redux/equipements/selectors";
 import { ActionEnum } from "../redux/equipements/DispatchType";
+import { Equipement } from "../../model/Equipement";
+import { Input, Select, Table } from "antd";
+import { useLoadEquipements } from "../hooks/useLoadEquipements";
+import { useState } from "react";
+import { GlobalTypeState } from "../redux/GlobalStateType";
+
+const { Option } = Select;
+
+const { Search } = Input;
 
 export function EquipementsTable() {
-  const equipements = useSelector(getAllEquipements);
+  useLoadEquipements();
   const dispatch = useDispatch();
+  const [option, setOption] = useState<"name" | "domain">("name");
+  const [text, setText] = useState("");
+  const onSearch = (searchedValue: string) => {
+    setText(searchedValue);
+  };
+  const equipements = useSelector((state) =>
+    getEquipementByOption(state as GlobalTypeState, { option, text })
+  );
+
+  const columns = [
+    {
+      title: "Photo",
+      dataIndex: "photo",
+      key: "photo",
+      render: (_: any, equipement: Equipement & { equipementKey: string }) => {
+        return (
+          <div>
+            <img
+              style={{
+                height: "271px",
+                width: "263px",
+              }}
+              alt="equipement"
+              src={equipement.photo}
+            />
+          </div>
+        );
+      },
+    },
+    {
+      title: "Nom de l'équipement",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Nombe de défault",
+      dataIndex: "nbFaults",
+      key: "nbFaults",
+    },
+    {
+      title: "domaine de l'équipement",
+      dataIndex: "domain",
+      key: "domain",
+    },
+    {
+      title: "Consulter",
+      dataIndex: "equipementKey",
+      key: "equipementKey",
+      render: (_: any, equipement: Equipement & { equipementKey: string }) => {
+        return (
+          <Link
+            onClick={() =>
+              dispatch({
+                type: ActionEnum.FETCH_EQUIPEMENT,
+                payload: equipement.equipementKey,
+              })
+            }
+            to={equipement.equipementKey}
+          >
+            Voir Détails
+          </Link>
+        );
+      },
+    },
+  ];
 
   return (
     <>
-      {equipements &&
-        Object.keys(equipements).map((key) => (
-          <div key={key}>
-            {key} ,,,,,
-            {
-              <Link
-                onClick={() =>
-                  dispatch({ type: ActionEnum.FETCH_EQUIPEMENT, payload: key })
-                }
-                to={key}
-              >
-                Voir Détails
-              </Link>
-            }
-          </div>
-        ))}
+      <Input.Group
+        style={{
+          display: "flex",
+          marginTop: "100px",
+          marginBottom: "50px",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        compact
+      >
+        <Select
+          onChange={(value) => {
+            setOption(value);
+          }}
+          defaultValue="name"
+        >
+          <Option value="name">Par Nom</Option>
+          <Option value="domain">Par Domain</Option>
+        </Select>
+        <Search
+          onSearch={onSearch}
+          placeholder="Recherche"
+          style={{ width: 200 }}
+          allowClear
+          enterButton
+        />
+      </Input.Group>
+      {equipements && (
+        <Table
+          columns={columns}
+          pagination={{
+            onChange: (page) => {
+              window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: "smooth",
+              });
+            },
+          }}
+          dataSource={equipements}
+          bordered
+        />
+      )}
     </>
   );
 }
