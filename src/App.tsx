7124aef157ad3e44d7b2021f45f7c773140392fa
LAKHAL Hamza <46263489+hamzalak@users.noreply.github.com>
2021-12-13 17:46:39 +0100

    global css and app

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,19 @@
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import { Layout } from "antd";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import { EquipementDescription } from "./app/Equipements/EquipementDescription";
-import { EquipementsTable } from "./app/Equipements/EquipementsTable";
-import { Home } from "./app/Home";
-import { ActionEnum } from "./app/redux/equipements/DispatchType";
-import { RoutesEnum } from "./app/utils/routeNames";
-import { database } from "./firebase";
-import { Equipement } from "./model/Equipement";
+import { Content } from "./app/Layout/Content";
+import { Footer } from "./app/Layout/Footer";
+import { Header } from "./app/Layout/Header";
 
 function App() {
-  const dispatch = useDispatch();
-  const [equipements, setEquipements] = useState<Record<string, Equipement>>();
-
-  useEffect(() => {
-    const getEquipements = async () => {
-      const ref = database.ref();
-      const equipements = await ref.child("Equipments").get();
-      setEquipements(equipements.val());
-    };
-    getEquipements();
-  }, []);
-
   return (
-    <Router>
-      <div>
-        <Link
-          onClick={() =>
-            dispatch({
-              type: ActionEnum.FETCH_ALL_EQUIPEMENTS,
-              payload: equipements,
-            })
-          }
-          to={RoutesEnum.EQUIPEMENTS}
-        >
-          Liste des Equipements
-        </Link>
-        <br />
-        <Link to={RoutesEnum.HOME}>Home</Link>
-        <br />
-      </div>
-      <Routes>
-        <Route
-          path={RoutesEnum.EQUIPEMENTS}
-          element={<EquipementsTable />}
-        ></Route>
-        <Route path={RoutesEnum.HOME} element={<Home />}></Route>
-        <Route
-          path={RoutesEnum.SINGLEEQUIPEMENT}
-          element={<EquipementDescription />}
-        />
-      </Routes>
-    </Router>
+    <Layout className="layout">
+      <Router>
+        <Header />
+        <Content />
+      </Router>
+      <Footer />
+    </Layout>
   );
 }
 export default App;
